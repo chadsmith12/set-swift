943bea24a34935eb69c6a9fde408bd2f24d50e53
Chad Smith <smith.chad12@gmail.com>
2021-06-08 22:57:55 -0500

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Chad Smith on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
